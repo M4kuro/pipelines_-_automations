a57fb57707952fe278d1181049d5ea783c14d339
M4kuro <leonardomapires@gmail.com>
2026-01-29 11:39:26 -0300

    Fixed transform file to a better practice approach

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from src.extract import extract_csv_files
-# from transform_std import transform_data
-from src.transform_vgsale import transform_sales_data
+#from src.transform import transform_data
+from src.transform import transform_vgsales_data
 from src.load import load_outputs
 
 df = extract_csv_files()
 #df = transform_data(df)
+df = transform_vgsales_data(df)
 
-df = transform_sales_data(df)
 load_outputs(df)
 print("ETL process completed!")
 print(df.head())
